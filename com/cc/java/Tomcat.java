5acdcf5fe85512124fd2ad01fa86b8930634fcd9
Sohrab <sohrab-arenja8@hotmail.de>
2023-07-31 11:05:22 +0200

    Super TomCat

---
+++ com/cc/java/Tomcat.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Tomcat extends SuperCat {
+
+
+  
+public Tomcat(String firstName, String furColor, int age) {
+  super(firstName, furColor, age);
+}
+
+  public String getAge() {
+    return String.valueOf(age);
+    }
+}
